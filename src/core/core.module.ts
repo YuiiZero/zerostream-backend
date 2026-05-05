15aa69e6b2c7cfc89fa831ac87459fdb9e623b92
yuii <romain.styajkin@yandex.ru>
2026-05-05 13:13:15 +0300

    fix: minor format fix

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
-import { PrismaModule } from './module/prisma/prisma.module';
+
+import { PrismaModule } from './module/prisma/prisma.module'
 
 @Module({
 	imports: [PrismaModule],
