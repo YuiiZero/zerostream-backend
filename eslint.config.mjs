40208c9cef8c09ee0c1a0b9b650fa5a58963b4ff
yuii <romain.styajkin@yandex.ru>
2026-05-06 19:53:48 +0300

    fix: remove exhaustive eslint indentation warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import prettier from 'eslint-config-prettier'
 
 export default defineConfig([
 	globalIgnores([
@@ -67,7 +67,6 @@ export default defineConfig([
 			'no-var': 'warn',
 
 			'arrow-parens': ['warn', 'as-needed'],
-			'indent': ['warn', 'tab'],
 			'comma-dangle': ['warn', 'never']
 		}
 	}
