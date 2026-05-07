6d09958dc20f414a86eac7d235ac98eadfe700a5
yuii <romain.styajkin@yandex.ru>
2026-05-07 18:45:52 +0300

    feat: config graphql

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,9 +1,12 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
 
 import { UserController } from '../module/user/user.controller'
 import { UserService } from '../module/user/user.service'
 
+import { getGraphqlConf } from './conf/getGraphqlConf'
 import { PrismaModule } from './module/prisma/prisma.module'
 import { RedisModule } from './module/redis/redis.module'
 
@@ -12,9 +15,16 @@ import { RedisModule } from './module/redis/redis.module'
 		PrismaModule,
 		ConfigModule.forRoot({
 			isGlobal: true,
+			// ignoreEnvFile: !IS_DEV,
 			envFilePath: '.env.dev'
 		}),
-		RedisModule
+		RedisModule,
+		GraphQLModule.forRootAsync<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			imports: [ConfigModule],
+			useFactory: getGraphqlConf,
+			inject: [ConfigService]
+		})
 	],
 	controllers: [UserController],
 	providers: [UserService]
